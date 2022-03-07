30f1d45e37bf11e46131e422e2ac84aad5767970
bambang12049 <73010048+bambang12049@users.noreply.github.com>
2022-03-07 21:47:36 +0700

    act3

---
+++ app/src/main/java/com/example/avt2/register.java
@@ -0,0 +1,30 @@
+package com.example.avt2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class register extends AppCompatActivity {
+    TextView tv_nama,tv_password;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        tv_nama=findViewById(R.id.tv_nama);
+        tv_password=findViewById(R.id.tv_pass);
+
+        Bundle c = getIntent().getExtras();
+
+        String nama=c.getString("a");
+        String pass=c.getString("b");
+
+        tv_nama.setText(nama);
+        tv_password.setText(pass);
+    }
+}
