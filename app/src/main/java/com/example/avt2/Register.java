fa033cff73904e57a9284b6b9e8e4c5e4b3278ed
bambang12049 <73010048+bambang12049@users.noreply.github.com>
2022-03-15 12:07:46 +0700

    act3

--- app/src/main/java/com/example/avt2/Register.java
+++ app/src/main/java/com/example/avt2/Register.java
@@ -3,28 +3,48 @@ package com.example.avt2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 public class Register extends AppCompatActivity {
-    TextView tv_nama,tv_password;
+    EditText edNama, edAlamat, edEmail, edPassword, edtrepass;
+
+    FloatingActionButton fab;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        edNama = findViewById(R.id.edNama);
+        edAlamat = findViewById(R.id.edAlamat);
+        edEmail = findViewById(R.id.edEmail);
+        edPassword = findViewById(R.id.edPass);
+        edtrepass = findViewById(R.id.edrepass);
+        fab = findViewById(R.id.fabSimpan);
 
-        tv_nama=findViewById(R.id.tv_nama);
-        tv_password=findViewById(R.id.tv_pass);
 
-        Bundle c = getIntent().getExtras();
-
-        String nama=c.getString("a");
-        String pass=c.getString("b");
-
-        tv_nama.setText(nama);
-        tv_password.setText(pass);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (edNama.getText().toString().isEmpty() ||
+                edAlamat.getText().toString().isEmpty() ||
+                edEmail.getText().toString().isEmpty() ||
+                edPassword.getText().toString().isEmpty() ||
+                edtrepass.getText().toString().isEmpty())
+                {
+                    Snackbar.make(view, "wajib isi", Snackbar.LENGTH_LONG).show();
+                }
+                else
+                {
+                    Snackbar.make(view,"password dan repass harus sama", Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
